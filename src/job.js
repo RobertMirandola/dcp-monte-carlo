074f684a42bd2b7a0e2d13c914b1561248f8dbcc
Robert Mirandola <robert@distributive.network>
2023-08-11 17:28:28 +0000

    cleanup frontend to remove legend

--- src/job.js
+++ src/job.js
@@ -5,21 +5,40 @@ async function main()
   const compute = require('dcp/compute');
   const fs = require('fs');
 
+  const file = 'out.json';
+
+    // Check if the file exists
+  fs.access(file, fs.constants.F_OK, (err) => {
+    if (err) {
+        console.error('File does not exist.');
+    } else {
+        // Delete the file
+        fs.unlink(file, (unlinkErr) => {
+            if (unlinkErr) {
+                console.error('Error deleting file:', unlinkErr);
+            } else {
+                console.log('File deleted successfully.');
+            }
+        });
+    }
+});
+
   function workFunction(numPoints)
   {
     progress();
-    var pointInCircle = false;
     var coordinates = [];
     for (let i = 0; i < numPoints; i++)
     {
+      var pointInCircle = false;
       const x = Math.random() * 2 - 1;
       const y = Math.random() * 2 - 1;
-      coordinates.push({x: x, y: y });
       const valToCompare = x*x + y*y;
 
       /* Check if point is in the quarter circle */
-      if (valToCompare < 0.5)
+      if (valToCompare < 1)
         pointInCircle = true;
+
+      coordinates.push({x: x, y: y, pointInCircle });
     }
     return coordinates;
   }
@@ -36,20 +55,15 @@ async function main()
     console.log(ev);
   })
 
-  job.on('result', (ev) => {
-    console.log(ev)
-  })
-
   const results = await job.exec();
   const resultsJSON = JSON.stringify(results.values());
 
 
-  const filePath = 'out.json';
-  fs.writeFile(filePath, resultsJSON, (err) => {
+  fs.writeFile(file, resultsJSON, (err) => {
     if (err) {
       console.error('Error writing JSON to file:', err);
   } else {
-      console.log('JSON data written to file:', filePath);
+      console.log('JSON data written to file:', file);
   }
   })
 
